be94f6278a7181b36de4ded40677351ad069d7b3
王萌(10029666) <wangm-s@glodon.com>
2023-09-25 10:53:30 +0800

    vllm api部署，调整参数解决生成问题结果异常bug

--- script/vllm/vllm在线部署.sh
+++ script/vllm/vllm在线部署.sh
@@ -2,10 +2,14 @@
 python -m vllm.entrypoints.api_server --model=/root/autodl-tmp/workspace/models/lora-merged-model/ --trust-remote-code --host=127.0.0.1 --port=10874
 
 # 模型调用
-curl http://localhost:10874/generate \
-> -d '{
-> "prompt": "李白是谁",
-> "use_beam_search": true,
-> "n": 4,
-> "temperature": 0
-> }'
+curl http://localhost:10874/generate -d \
+'{
+  "prompt": "[INST] <<SYS>>\nYou are a helpful assistant. 你是一个乐于助人的助手，请基于你的知识用中文回答当前问题。\n<</SYS>>\n\n这是本次的问题：请简述人工智能的未来\n [/INST]",
+  "n": 1,
+  "top_p": 0.9,
+  "top_k": 40,
+  "temperature": 0.2,
+  "frequency_penalty": 1.2,
+  "max_tokens": 1024
+}'
+
